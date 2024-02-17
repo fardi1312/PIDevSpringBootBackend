2f0e76bfec27748d67c19adb0ca78fcf398e9882
MASSOUD <Massoud.fradi@esprit.tn>
2024-02-17 20:15:22 +0100

    Initial commit

---
+++ src/main/java/tn/esprit/pidevspringbootbackend/PiDevSpringBootBackendApplication.java
@@ -0,0 +1,13 @@
+package tn.esprit.pidevspringbootbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiDevSpringBootBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PiDevSpringBootBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tn/esprit/pidevspringbootbackend/PiDevSpringBootBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.esprit.pidevspringbootbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiDevSpringBootBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
